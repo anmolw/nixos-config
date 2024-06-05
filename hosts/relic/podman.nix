c552b57e3b185717d1367bd3bee190bd48df62b0
Anmol Wadhwani <anmolsw@gmail.com>
2024-06-05 10:31:19 +0530

    Added podman to relic

---
+++ hosts/relic/podman.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  virtualisation.containers.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    autoPrune.enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    podman-compose
+    podman-tui
+  ];
+}
