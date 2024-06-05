3f63f772dc14a99275c63216dbd39a7ef84be584
Anmol Wadhwani <anmolsw@gmail.com>
2024-06-05 07:32:11 +0530

    Initial media server config

---
+++ hosts/relic/ksmbd.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  # ksmbd
+  environment.systemPackages = with pkgs; [ksmbd-tools];
+
+  environment.etc."ksmbd/ksmbd.conf" = {
+    mode = "0640";
+    text = ''
+      ; see ksmbd.conf(5) for details
+      [global]
+        workgroup = WORKGROUP
+        netbios name = relic
+
+      [media]
+        ; share parameters
+        comment = Media drive
+        create mask = 0755
+        force group = media
+        force user = anmol
+        path = /srv/media
+        read only = yes
+        write list = anmol
+        guest ok = yes
+        guest account = guest
+    '';
+  };
+
+  networking.firewall.allowedTCPPorts = [ 445 ];
+  systemd.packages = with pkgs; [ksmbd-tools];
+  systemd.services.ksmbd.wantedBy = ["multi-user.target"];
+}
---
+++ hosts/relic/network.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  networking.hostName = "relic"; # Define your hostname.
+  networking.interfaces.eno1.ipv4.addresses = [
+    {
+      address = "192.168.29.120";
+      prefixLength = 24;
+    }
+  ];
+  networking.nameservers = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+  networking.defaultGateway = {
+    address = "192.168.29.1";
+    interface = "eno1";
+  };
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = ["~."];
+    fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+    dnsovertls = "true";
+  };
+}
