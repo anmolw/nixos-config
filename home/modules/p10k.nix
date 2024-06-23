fe3a613802dbe12fdda3827db515e12e7966bcf6
Anmol Wadhwani <anmolsw@gmail.com>
2024-06-23 23:59:41 +0000

    Enable powerlevel10k instant prompt

--- home/modules/p10k.nix
+++ home/modules/p10k.nix
@@ -7,6 +7,15 @@
     }
   ];
 
+  programs.zsh.initExtraFirst = ''
+    # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+    # Initialization code that may require console input (password prompts, [y/n]
+    # confirmations, etc.) must go above this block; everything else may go below.
+    if [[ -r "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh" ]]; then
+      source "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh"
+    fi
+  '';
+
   programs.zsh.initExtra = ''
     test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh
   '';
