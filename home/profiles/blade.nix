359a3ca07cb1c587f4504bb31e982cee2dee6ca1
anmolw <4815989+anmolw@users.noreply.github.com>
2024-06-24 10:24:19 +0530

    Revert blade kernel to stock, fix state version

--- home/profiles/blade.nix
+++ home/profiles/blade.nix
@@ -8,10 +8,13 @@
     ../modules/shell.nix
     ../modules/development.nix
     ../modules/ssh.nix
+    ../modules/fonts.nix
     ../modules/vscode.nix
     ../modules/krisp.nix
     ../modules/nixos-specific.nix
   ];
+
+  home.stateVersion = "24.05";
 
   programs.discord.enable = true;
   programs.discord.wrapDiscord = true;
