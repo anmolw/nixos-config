59c39f070350357423049d37b4d7a56312fbf901
anmolw <4815989+anmolw@users.noreply.github.com>
2024-07-01 09:24:56 +0000

    Added dua and erdtree to shell

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -1,5 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    dua
+    erdtree
     fzf
   ];
 
--- nixos/hosts/blade/configuration.nix
+++ nixos/hosts/blade/configuration.nix
@@ -46,7 +46,7 @@
 
   services.resolved = {
     enable = true;
-    dnsovertls = "true";
+    dnsovertls = "opportunistic";
     fallbackDns = [
       "1.1.1.1#one.one.one.one"
       "1.0.0.1#one.one.one.one"
