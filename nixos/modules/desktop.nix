5a118e96b31724b23d8ac60dd87312d86a25d42a
anmolw <4815989+anmolw@users.noreply.github.com>
2024-08-02 12:56:14 +0530

    Use wayland for sddm

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -3,8 +3,13 @@
   services.xserver.enable = true;
 
   # Enable the Plasma 6 Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.enableHidpi = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    wayland.compositor = "kwin";
+    enableHidpi = true;
+  };
+
   services.desktopManager.plasma6.enable = true;
 
   environment.systemPackages = with pkgs; [
