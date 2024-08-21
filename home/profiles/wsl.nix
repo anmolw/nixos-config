a8864766ebba3f54dc2e244e7e752c56c4cd9e24
Anmol Wadhwani <anmolsw@gmail.com>
2024-08-22 03:39:47 +0530

    Add charm.sh apps

--- home/profiles/wsl.nix
+++ home/profiles/wsl.nix
@@ -40,6 +40,9 @@ in {
     nh
     uv
     sops
+    gum
+    wishlist
+    mods
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
