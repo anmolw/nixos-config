9a107f832fe215c694ac27c9afcda5c93af742c7
anmolw <4815989+anmolw@users.noreply.github.com>
2024-09-07 00:34:10 +0530

    fixup! Add Fira Code NF

--- home/modules/fonts.nix
+++ home/modules/fonts.nix
@@ -2,6 +2,6 @@
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
     fira-code
-    (nerdfonts.override {fonts = ["Hack" "Meslo" "Fira Code"];})
+    (nerdfonts.override {fonts = ["Hack" "Meslo" "FiraCode"];})
   ];
 }
