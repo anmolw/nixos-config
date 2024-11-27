70fa70ef138f27d5383d63bb1b0b67717d3f989d
anmolw <4815989+anmolw@users.noreply.github.com>
2024-11-28 02:22:49 +0530

    hosts: Add pi

--- flake.nix
+++ flake.nix
@@ -110,5 +110,15 @@
             ];
           };
         };
+
+      nixosConfigurations.pi = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        system = "aarch64-linux";
+        modules = [
+          inputs.nixos-hardware.nixosModules.raspberry-pi-4
+          home-manager.nixosModules.default
+          ./hosts/pi/configuration.nix
+        ];
+      };
     };
 }
