b15086aa4f771a3f508f77f00ebaf071d273b1c5
anmolw <4815989+anmolw@users.noreply.github.com>
2024-11-28 02:22:49 +0530

    Add pi config
    WIP

---
+++ hosts/pi/network.nix
@@ -0,0 +1,29 @@
+{
+  networking.hostName = "pi"; # Define your hostname.
+  networking.interfaces.eno1.ipv4.addresses = [
+    {
+      address = "192.168.29.100";
+      prefixLength = 24;
+    }
+  ];
+  networking.nameservers = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+  networking.defaultGateway = {
+    address = "192.168.29.1";
+    interface = "eno1";
+  };
+
+  services.resolved = {
+    enable = true;
+    dnssec = true;
+    domains = ["~."];
+    fallbackDns = [
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+      "9.9.9.9#dns.quad9.net"
+    ];
+    dnsovertls = "opportunistic";
+  };
+
+  networking.firewall.allowedTCPPorts = [];
+  networking.firewall.allowedUDPPorts = [];
+}
