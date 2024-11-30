bb4df6e48299762d18470a1c24b2d26b1d398e4e
anmolw <4815989+anmolw@users.noreply.github.com>
2024-11-30 05:30:45 +0530

    Move p10k config into zsh module

--- modules/home/shell/zsh.nix
+++ modules/home/shell/zsh.nix
@@ -3,23 +3,46 @@
   lib,
   config,
   ...
-}: {
-  imports = [
-    ./p10k.nix
-  ];
+}: let
+  cfg = config.programs.zsh;
+in {
+  options.programs.zsh.p10k.enable = lib.mkEnableOption "Enable powerlevel10k";
 
-  programs.zsh = {
-    enable = true;
-    dotDir = ".config/zsh";
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    plugins = [
-      {
-        name = "fast-syntax-highlighting";
-        src = pkgs.zsh-fast-syntax-highlighting;
-        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
-      }
-    ];
-    # syntaxHighlighting.enable = true;
-  };
+  config.programs.zsh = lib.mkMerge [
+    {
+      enable = true;
+      dotDir = ".config/zsh";
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      plugins = [
+        {
+          name = "fast-syntax-highlighting";
+          src = pkgs.zsh-fast-syntax-highlighting;
+          file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+        }
+      ];
+    }
+    (lib.mkIf (cfg.enable && cfg.p10k.enable) {
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+      ];
+
+      initExtraFirst = ''
+        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+        # Initialization code that may require console input (password prompts, [y/n]
+        # confirmations, etc.) must go above this block; everything else may go below.
+        if [[ -r "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh" ]]; then
+          source "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh"
+        fi
+      '';
+
+      initExtra = ''
+        test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh
+      '';
+    })
+  ];
 }
