213d60faf5221fa872520f57430fc3a6dd6c8dd0
anmolw <4815989+anmolw@users.noreply.github.com>
2025-02-20 21:17:37 +0530

    Allow disabling default fish plugins Also disable the `done` plugin on WSL

--- modules/home/shell/fish.nix
+++ modules/home/shell/fish.nix
@@ -1,41 +1,57 @@
-{ pkgs, ... }:
 {
-  programs.fish = {
-    enable = true;
-    shellInit = ''
-      set fish_greeting
-    '';
-    plugins = [
-      {
-        name = "tide";
-        src = pkgs.fishPlugins.tide.src;
-      }
-      {
-        name = "autopairs";
-        src = pkgs.fishPlugins.autopair.src;
-      }
-      {
-        name = "done";
-        src = pkgs.fishPlugins.done.src;
-      }
-      {
-        name = "bangbang";
-        src = pkgs.fetchFromGitHub {
-          owner = "oh-my-fish";
-          repo = "plugin-bang-bang";
-          rev = "ec991b80ba7d4dda7a962167b036efc5c2d79419";
-          sha256 = "sha256-oPPCtFN2DPuM//c48SXb4TrFRjJtccg0YPXcAo0Lxq0=";
-        };
-      }
-    ];
-    shellAbbrs = {
-      g = "git";
-      ga = "git add";
-      gco = "git checkout";
-      gd = "git diff";
-      gds = "git diff --staged";
-      gl = "git log";
-      gs = "git status";
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  pluginIsDisabled = plugin: builtins.elem plugin.name config.programs.fish.disabledPlugins;
+  defaultPlugins = [
+    {
+      name = "tide";
+      src = pkgs.fishPlugins.tide.src;
+    }
+    {
+      name = "autopairs";
+      src = pkgs.fishPlugins.autopair.src;
+    }
+    {
+      name = "done";
+      src = pkgs.fishPlugins.done.src;
+    }
+    {
+      name = "bangbang";
+      src = pkgs.fetchFromGitHub {
+        owner = "oh-my-fish";
+        repo = "plugin-bang-bang";
+        rev = "ec991b80ba7d4dda7a962167b036efc5c2d79419";
+        sha256 = "sha256-oPPCtFN2DPuM//c48SXb4TrFRjJtccg0YPXcAo0Lxq0=";
+      };
+    }
+  ];
+in
+{
+  options.programs.fish.disabledPlugins = lib.mkOption {
+    description = "List of plugins to disable.";
+    default = [ ];
+  };
+
+  config = {
+    programs.fish = {
+      enable = true;
+      shellInit = ''
+        set fish_greeting
+      '';
+      plugins = builtins.filter (plugin: !pluginIsDisabled plugin) defaultPlugins;
+      shellAbbrs = {
+        g = "git";
+        ga = "git add";
+        gco = "git checkout";
+        gd = "git diff";
+        gds = "git diff --staged";
+        gl = "git log";
+        gs = "git status";
+      };
     };
   };
 }
