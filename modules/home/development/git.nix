e0478dd8c3a2cd04731fc5f4441068a0a864b7ed
anmolw <4815989+anmolw@users.noreply.github.com>
2025-03-03 04:29:32 +0530

    Set up commit signing

--- modules/home/development/git.nix
+++ modules/home/development/git.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   programs.git = {
     enable = true;
@@ -14,7 +15,11 @@
       user = {
         name = "anmolw";
         email = "4815989+anmolw@users.noreply.github.com";
+        signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINhjKonstq1xifZcoYcPkaqZmD6hOLMRdzq7xfhFrPLm anmol@desktop";
       };
+      commit.gpgsign = true;
+      tag.gpgsign = true;
+      gpg.format = "ssh";
       init.defaultBranch = "main";
     };
   };
