3a1f95564fae7dc537b2f222941333c667f1f5da
anmolw <4815989+anmolw@users.noreply.github.com>
2025-03-09 09:03:20 +0530

    Relic: Update nfs subnet

--- hosts/relic/nfs.nix
+++ hosts/relic/nfs.nix
@@ -1,7 +1,7 @@
 {
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /srv/media -mp *(ro) 192.168.29.0/24(rw,all_squash,anonuid=1000,anongid=1002)
+    /srv/media -mp *(ro) 192.168.1.0/24(rw,all_squash,anonuid=1000,anongid=1002)
   '';
 
   networking.firewall.allowedTCPPorts = [ 2049 ];
