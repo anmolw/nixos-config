5fec18b5e9ca45e9b879348f0270c11646b8b766
anmolw <4815989+anmolw@users.noreply.github.com>
2025-03-11 21:14:41 +0530

    Use systemd-resolved for mDNS

--- hosts/blade/network.nix
+++ hosts/blade/network.nix
@@ -1,34 +1,29 @@
 {
   networking = {
     hostName = "blade"; # Define your hostname.
-    # Pick only one of the below networking options.
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networkmanager.enable = true; # Easiest to use and most distros use this by default.
     networkmanager.dns = "systemd-resolved";
-    # Open ports in the firewall.
     firewall.allowedTCPPorts = [
       57621 # spotify
     ];
     firewall.allowedUDPPorts = [
-      5353 # spotify
+      5353 # mDNS
     ];
-  };
-
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
   };
 
   services.resolved = {
     enable = true;
     dnsovertls = "opportunistic";
+    dnssec = "true";
+    domains = [ "~." ];
+    extraConfig = ''
+      MulticastDNS = "true"
+    '';
     fallbackDns = [
       "1.1.1.1#one.one.one.one"
       "1.0.0.1#one.one.one.one"
       "9.9.9.9#dns.quad9.net"
     ];
-    domains = [ "~." ];
-    dnssec = "true";
   };
 
   security.polkit.extraConfig = ''
--- hosts/relic/network.nix
+++ hosts/relic/network.nix
@@ -1,29 +1,29 @@
 {
   networking = {
-    hostName = "relic"; # Define your hostname.
+    hostName = "relic";
     interfaces.eno1.useDHCP = true;
     nameservers = [
       "1.1.1.1#one.one.one.one"
       "1.0.0.1#one.one.one.one"
     ];
     firewall.allowedTCPPorts = [ ];
-    firewall.allowedUDPPorts = [ ];
-  };
-
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
+    firewall.allowedUDPPorts = [
+      5353 # mDNS
+    ];
   };
 
   services.resolved = {
     enable = true;
+    dnsovertls = "opportunistic";
     dnssec = "true";
     domains = [ "~." ];
+    extraConfig = ''
+      MulticastDNS = "true"
+    '';
     fallbackDns = [
       "1.1.1.1#one.one.one.one"
       "1.0.0.1#one.one.one.one"
       "9.9.9.9#dns.quad9.net"
     ];
-    dnsovertls = "opportunistic";
   };
 }
