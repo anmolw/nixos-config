7fd76154a3b19545c4d24f350dd7f209aa3ade20
anmolw <4815989+anmolw@users.noreply.github.com>
2025-03-11 22:41:59 +0530

    relic: Use systemd-networkd instead of scripted networking

--- hosts/relic/network.nix
+++ hosts/relic/network.nix
@@ -1,15 +1,41 @@
 {
   networking = {
     hostName = "relic";
-    interfaces.eno1.useDHCP = true;
-    nameservers = [
-      "1.1.1.1#one.one.one.one"
-      "1.0.0.1#one.one.one.one"
-    ];
+    useDHCP = false;
+    dhcpcd.enable = false;
     firewall.allowedTCPPorts = [ ];
     firewall.allowedUDPPorts = [
       5353 # mDNS
     ];
+  };
+
+  systemd.network = {
+    enable = true;
+    networks."10-lan" = {
+      networkConfig = {
+        DHCP = "yes";
+        IPv6AcceptRA = true;
+        DNSOverTLS = true;
+        MulticastDNS = true;
+      };
+      dhcpV4Config = {
+        UseDNS = false;
+      };
+      dhcpV6Config = {
+        UseDNS = false;
+      };
+      ipv6AcceptRAConfig = {
+        UseDNS = false;
+      };
+      dns = [
+        "1.1.1.1#one.one.one.one"
+        "1.0.0.1#one.one.one.one"
+        "2606:4700:4700::1111#one.one.one.one"
+        "2606:4700:4700::1001#one.one.one.one"
+      ];
+      matchConfig.Name = "eno1";
+      linkConfig.RequiredForOnline = "routable";
+    };
   };
 
   services.resolved = {
