34915a411701b4bae9decbdcc07433cad1bc3195
anmolw <4815989+anmolw@users.noreply.github.com>
2025-03-16 17:01:51 +0530

    relic: Update jellyfin settings

--- hosts/relic/jellyfin.nix
+++ hosts/relic/jellyfin.nix
@@ -1,8 +1,16 @@
+{ pkgs, ... }:
 {
   services.jellyfin = {
     enable = true;
+    user = "jellyfin";
+    group = "jellyfin";
     openFirewall = true;
   };
+  environment.systemPackages = with pkgs; [
+    jellyfin
+    jellyfin-web
+    jellyfin-ffmpeg
+  ];
 
   users.users.jellyfin.uid = 1100;
   users.users.anmol.extraGroups = [ "jellyfin" ];
