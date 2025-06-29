e3b9535ab9f4af9e709bb431f417aa3c8b7ee34d
anmolw <4815989+anmolw@users.noreply.github.com>
2025-06-30 01:58:38 +0530

    desktop config

---
+++ hosts/desktop/configuration.nix
@@ -0,0 +1,104 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ../../modules/nixos/nixsettings.nix
+    ../../modules/nixos/podman.nix
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+  boot.extraModulePackages = [ config.boot.kernelPackages.xone ];
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
+    pulse.enable = true;
+    extraConfig.pipewire = { };
+  };
+
+  programs.fish.enable = true;
+  programs.nh = {
+    enable = true;
+    flake = "/home/anmol/code/github/nixos-config";
+  };
+
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    aria2
+    bat
+    compsize
+    croc
+    curl
+    eza
+    fd
+    file
+    fzf
+    git
+    jujutsu
+    micro
+    nvme-cli
+    pciutils
+    smartmontools
+    usbutils
+    vim
+    wget
+    wireguard-tools
+  ];
+
+  programs.firefox.enable = true;
+
+  programs.steam = {
+    enable = true;
+    protontricks.enable = true;
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
+  };
+
+  services.fwupd.enable = true;
+  services.smartd.enable = true;
+
+  services.flatpak.enable = true;
+
+  services.tailscale.enable = true;
+
+  users.users.anmol = {
+    isNormalUser = true;
+    shell = config.programs.fish.package;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP5QyKja6UgJW2DrXEFgbtgNZoJlinEvTVpcZy6EfnbK anmol@blade"
+    ];
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+    packages = [ ];
+  };
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.PermitRootLogin = "no";
+    settings.KbdInteractiveAuthentication = false;
+    openFirewall = true;
+  };
+
+  virtualisation.vmVariant = { };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "Asia/Kolkata";
+
+  system.stateVersion = "25.11";
+}
---
+++ hosts/desktop/hardware-configuration.nix
@@ -0,0 +1,71 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "usb_storage"
+    "usbhid"
+    "sd_mod"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems = {
+    "/boot" = {
+      fsType = "vfat";
+      device = "/dev/disk/by-uuid/5A38-FE95";
+    };
+    "/" = {
+      options = [
+        "subvol=@root"
+        "compress=zstd:1"
+      ];
+      device = "/dev/disk/by-uuid/238023bf-e0b3-47fb-be73-37b720da7e3b";
+    };
+    "/home" = {
+      fsType = "btrfs";
+      options = [
+        "subvol=@home"
+        "compress=zstd:1"
+      ];
+      device = "/dev/disk/by-uuid/238023bf-e0b3-47fb-be73-37b720da7e3b";
+    };
+    "/nix" = {
+      options = [
+        "subvol=@nix"
+        "compress=zstd:1"
+      ];
+      device = "/dev/disk/by-uuid/238023bf-e0b3-47fb-be73-37b720da7e3b";
+    };
+    "/var" = {
+      options = [
+        "subvol=@var"
+        "compress=zstd:1"
+      ];
+      device = "/dev/disk/by-uuid/238023bf-e0b3-47fb-be73-37b720da7e3b";
+    };
+    "/tmp" = {
+      options = [
+        "subvol=@tmp"
+        "compress=zstd:1"
+      ];
+      device = "/dev/disk/by-uuid/238023bf-e0b3-47fb-be73-37b720da7e3b";
+    };
+  };
+
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
