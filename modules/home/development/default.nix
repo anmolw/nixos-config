7dfb8bd62f6a9be3a09a48177b9e5008852b1b34
anmolw <4815989+anmolw@users.noreply.github.com>
2025-07-03 14:53:45 +0530

    hm: split helix config

--- modules/home/development/default.nix
+++ modules/home/development/default.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }:
 {
   imports = [
-    ./git.nix
-    ./jj.nix
     ./delta.nix
+    ./git.nix
+    ./helix.nix
+    ./jj.nix
   ];
 
   home.packages = with pkgs; [
@@ -72,34 +73,5 @@
       }
     '';
     nix-direnv.enable = true;
-  };
-
-  programs.helix = {
-    enable = true;
-    settings = {
-      theme = "catppuccin_mocha";
-      editor = {
-        line-number = "relative";
-        cursor-shape = {
-          insert = "bar";
-          normal = "block";
-          select = "block";
-        };
-        end-of-line-diagnostics = "hint";
-        true-color = true;
-        # show warnings and errors on the cursorline inline
-        inline-diagnostics.cursor-line = "warning";
-      };
-    };
-    languages = {
-      language = [
-        {
-          name = "nix";
-          formatter = {
-            command = "nixfmt";
-          };
-        }
-      ];
-    };
   };
 }
