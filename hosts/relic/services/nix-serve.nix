dd7548bee11cfcd928559fcd16c6d404c232ea98
anmolw <4815989+anmolw@users.noreply.github.com>
2025-07-05 16:41:36 +0530

    replace nix-serve with nix-serve-ng

--- hosts/relic/services/nix-serve.nix
+++ hosts/relic/services/nix-serve.nix
@@ -1,8 +1,9 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   # Binary cache for other machines
   services.nix-serve = {
     enable = true;
+    package = pkgs.nix-serve-ng;
     secretKeyFile = config.sops.secrets."nix-serve-priv-key".path;
     openFirewall = true;
   };
