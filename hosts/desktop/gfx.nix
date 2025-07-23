d61c2ff6f29fdebd7e1eb92239b7a46585f25f2d
anmolw <4815989+anmolw@users.noreply.github.com>
2025-07-23 19:47:54 +0530

    desktop: disable amdvlk

--- hosts/desktop/gfx.nix
+++ hosts/desktop/gfx.nix
@@ -4,13 +4,13 @@
     enable = true;
     enable32Bit = true;
     extraPackages = with pkgs; [
-      amdvlk
+      # amdvlk
       rocmPackages.clr.icd
     ];
-    extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+    # extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
   };
   environment.variables = {
-    AMD_VULKAN_ICD = "RADV";
+    # AMD_VULKAN_ICD = "RADV";
     LIBVA_DRIVER_NAME = "radeonsi";
   };
   environment.systemPackages = with pkgs; [ lact ];
