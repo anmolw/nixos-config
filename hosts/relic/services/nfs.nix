33a913768c3fc42a790ec567f72fb1530f27a971
anmolw <4815989+anmolw@users.noreply.github.com>
2025-09-15 22:07:44 +0530

    relic: update nfs settings

--- hosts/relic/services/nfs.nix
+++ hosts/relic/services/nfs.nix
@@ -1,7 +1,7 @@
 {
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /srv/media -mp *(ro) 192.168.1.0/24(rw,all_squash,anonuid=1000,anongid=1100) *.local(rw,all_squash,anonuid=1000,anongid=1100)
+    /srv/media -mp *(ro) 192.168.1.0/24(rw,all_squash,anonuid=1002,anongid=1002) *.local(rw,all_squash,anonuid=1002,anongid=1002)
   '';
 
   networking.firewall.allowedTCPPorts = [ 2049 ];
